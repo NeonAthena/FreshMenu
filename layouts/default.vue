52cd044d3c754cc92c5717ab0049353a83568288
BaniKaurGauri <banikaur.gauri08@nmims.edu.in>
2020-09-16 14:48:53 +0530

    Trial 2

---
+++ layouts/default.vue
@@ -0,0 +1,44 @@
+<template>
+  <!--Your App Starts Here -->
+  <v-app id="inspire">
+    
+    <!--Header -->
+    <Densetoolbar/>
+    <Breadcrumbs/>
+      <!-- <v-app-bar color="red" dark fixed app>
+       <v-toolbar-title>Application</v-toolbar-title>
+      
+       <VuetifyLogo/> 
+       
+    </v-app-bar> -->
+
+    <!-- Body -->
+   
+    <v-content light>
+      <nuxt /> 
+    </v-content>
+  
+    <!-- Footer 
+    <iconsfooter/>
+  
+    <v-footer color="indigo" app>
+      <span class="white--text">&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>-->
+
+  </v-app>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style> 
+  .white--text {
+    border: 1px solid;
+  }
+
+   #inspire {
+    background-color: white;
+    color: black;
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <v-content>
+      <Grids/>
+      <br><br>
+      <Grids2/>
+      <br>
+      <br>
+      <Customaction/>
+      <br>
+      <br>
+      <Loadingcard/>
+    </v-content>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style> 
+</style>
